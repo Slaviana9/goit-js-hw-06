9df7e7ee33ae05a69a0415a76f8b605b4fc8e919
Slaviana9 <davidukslavana@gmail.com>
2024-01-18 20:47:31 +0000

    added tasks

---
+++ js/tack-2.js
@@ -0,0 +1,27 @@
+class Storage {
+  #items;
+
+  constructor(item) {
+    this.#items = item;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    this.#items.splice(itemToRemove, 1);
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
